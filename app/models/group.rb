0bf7b484e1da25d308e912e8625e299402ed3b59
Fedriek Chandra <fedriekchandra@gmail.com>
2020-12-20 21:48:19 +0700

    update

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
     has_one_attached :logo
-    has_many :group_has_pic
+    has_many :group_has_pic, dependent: :destroy
+    has_many :group_has_managers, dependent: :destroy
 end
